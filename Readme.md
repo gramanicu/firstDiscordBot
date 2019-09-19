562bdb5c113627f3d5039fbe7779511b027f1e76
Grama Nicolae <gramanicu@gmail.com>
2019-09-19 10:34:28 +0300

    Solved some tslint erorrs in index

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 # Fist Discord Bot - [![Build Status](https://travis-ci.com/gramanicu/firstDiscordBot.svg?branch=master)](https://travis-ci.com/gramanicu/firstDiscordBot) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/c8cc752c20b642329609ca221986acdf)](https://www.codacy.com/manual/gramanicu/firstDiscordBot?utm_source=github.com&utm_medium=referral&utm_content=gramanicu/firstDiscordBot&utm_campaign=Badge_Grade) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-![Logo](./Images/BlueBotSmall.png "Logo")
+![Logo](./images/BlueBotSmall.png "Logo")
 
 This is a project to learn how to create Discord bots
 
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
-import * as Discord from "discord.js";
-import * as ConfigFile from "./config";
-import { IBotCommand } from "./api";
+import * as Discord from 'discord.js';
+import { IBotCommand } from './api';
+import * as ConfigFile from './config';
 
 const client: Discord.Client = new Discord.Client();
 
-
 const loadCommands = (commandsPath: string) => {
-    let cmds: IBotCommand[] = [];
+    const cmds: IBotCommand[] = [];
     // Exit if there are no config file or no commands
-    if(!ConfigFile.config || (ConfigFile.config.commands as string[]).length === 0) { return; }
+    if (!ConfigFile.config || (ConfigFile.config.commands as string[]).length === 0) { return; }
 
     // Loop through all of the commands in our config file
-    for(const commandName of ConfigFile.config.commands as string[]) {
+    for (const commandName of ConfigFile.config.commands as string[]) {
         const commandClass = require(`${commandsPath}/${commandName}`).default;
         const command = new commandClass() as IBotCommand;
 
@@ -19,73 +18,73 @@ const loadCommands = (commandsPath: string) => {
     }
 
     return cmds;
-}
+};
 
 const commands: IBotCommand[] = loadCommands(`${__dirname}/commands`);
 
-
-client.on("ready", () => {
+client.on('ready', () => {
     // Set the bot's activity
-    console.log("Bot is online!");
-    client.user.setActivity("Papa Pewds", {type: "WATCHING"});
-})
+    process.stdout.write('Bot is online!');
+    client.user.setActivity('Papa Pewds', {type: 'WATCHING'});
+});
 
-client.on("guildMemberAdd", member => {
+client.on('guildMemberAdd', (member) => {
     // Displays a welcome message on the specified channel when a new user joins
-    const welcomeChannelName = "general";
-    const welcomeChannel = member.guild.channels.find(channel => channel.name === welcomeChannelName) as Discord.TextChannel;
+    const welcomeChannelName = 'general';
+    const welcomeChannel = member.guild.channels.find((channel) => channel.name
+                                === welcomeChannelName) as Discord.TextChannel;
     welcomeChannel.send(`Welcome ${member.displayName}!`);
 
     // Adds a role to the new member
-    const memberRole = member.guild.roles.find(role => role.id == "623891768362008610");
+    const memberRole = member.guild.roles.find((role) => role.id === '623891768362008610');
     member.addRole(memberRole);
 
     // Sends a DM to the member
-    member.send("Thank you for joining our server!");
-})
+    member.send('Thank you for joining our server!');
+});
 
-client.on("guildMemberRemove", member => {
-    const welcomeChannel = member.guild.channels.find(channel => channel.name === "github") as Discord.TextChannel;
+client.on('guildMemberRemove', (member) => {
+    const welcomeChannel = member.guild.channels.find((channel) => channel.name === 'github') as Discord.TextChannel;
     welcomeChannel.send(`We are sorry that you had to go :( ${member.displayName}!`);
-})
+});
 
-client.on("message", msg => {
+client.on('message', (msg) => {
     // Ignore the message if it was sent by the bot
-    if(msg.author.bot) { return; }
+    if (msg.author.bot) { return; }
 
     // Ignore direct messages
-    if(msg.channel.type == "dm") { return; }
+    if (msg.channel.type === 'dm') { return; }
 
     // Ignore messages that don't start with the prefix
-    if(!msg.content.startsWith(ConfigFile.config.prefix)) { return; }
+    if (!msg.content.startsWith(ConfigFile.config.prefix)) { return; }
 
     // Handle the command
     handleCommand(msg);
-})
+});
 
-const handleCommand = async (msg: Discord.Message) => {
+const handleCommand = async(msg: Discord.Message) => {
     // Split the string into the command and all of the args
-    let command = msg.content.split(" ")[0].replace(ConfigFile.config.prefix, "").toLowerCase();
-    const args = msg.content.split(" ").slice(1);
+    const command = msg.content.split(' ')[0].replace(ConfigFile.config.prefix, '').toLowerCase();
+    const args = msg.content.split(' ').slice(1);
 
      // Loop through all the loaded commands
-    for(const commandClass of commands) {
+    for (const commandClass of commands) {
         // Attempt to execute code
         try {
             // Check if the command is valid
-            if(!commandClass.isThisCommand(command)) {
+            if (!commandClass.isThisCommand(command)) {
                 // Go to the next iteration if not
                 continue;
             }
 
             // Pause execution while running the command
             await commandClass.runCommand(args, msg, client);
-        } catch(exception) {
+        } catch (exception) {
             // If there is an error, log it
-            console.log(exception);
+            process.stdout.write(exception);
         }
     }
-}
+};
 
 // Login the bot into the server
 client.login(process.env.DISCORD_BOT_TOKEN || ConfigFile.config.token);
