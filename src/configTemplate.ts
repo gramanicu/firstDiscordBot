130260a1557fc76b8035e88e95c59cc6f438a7de
Grama Nicolae <gramanicu@gmail.com>
2019-09-19 10:37:23 +0300

    Linting errors solved api and configTemplate

--- src/configTemplate.ts
+++ src/configTemplate.ts
@@ -1,11 +1,11 @@
 export let config = {
-    "token": "",    // The bot key
-    "prefix": "!",         // Command prefix,
-    "commands": [           // The commands for the bot
-        "kick",
-        "ban",
-        "purge",
-        "serverinfo",
-        "vote"
+    commands: [          // The commands for the bot
+        'kick',
+        'ban',
+        'purge',
+        'serverinfo',
+        'vote',
     ],
-}
+    prefix: '!',         // Command prefix,
+    token: '',           // The bot key
+};
